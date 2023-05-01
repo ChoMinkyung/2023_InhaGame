257fd57b1dfe0724980a9f92f56c4312189f4a37
ChoMinkyung <58170545+ChoMinkyung@users.noreply.github.com>
2023-05-01 12:06:26 +0900

    20230501 Q2, Q3 문제 풀이

--- ConsoleApplication1/20230501.c
+++ ConsoleApplication1/20230501.c
@@ -1,28 +1,39 @@
-//charter 06-2 반복문 활용
-#define _crt_secure_no_warnings
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-int main() 
-{
-	long long n;
-	long long answer = 0;
-	printf("n을 입력하세요 : ");
-	scanf("%lld", &n);
-	long long n2 = n;
-	if (n > 1000000000000) n2 = n / 1000000;
-	else n2 = n / 2;
-
-	for (int i = 1; i <= n2; i++)
-	{
-		if (i == (long long)n / i)
-		{
-			answer = (i + 1) * (i + 1);
-		}
-		if (answer == 0) answer = -1;
-	}
-	printf("answer = %lld\n", answer);
-}
+////charter 06-2 반복문 활용
+//#define _crt_secure_no_warnings
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//
+//
+//int main()
+//{
+//    int x;
+//    printf("x를 입력하세요 : ");
+//    scanf_s("%d", &x);
+//    bool answer = true;
+//    int x4=0, x3=0, x2=0, x1=0;
+//    if ((x / 1000) > 0)
+//    {
+//        x4 = x / 1000;
+//        printf("x4:%d ", x4);
+//    }
+//    if ((x / 100) > 0)
+//    {
+//        x3 = (x-x4*1000) / 100;
+//        printf("x3:%d ", x3);
+//    }
+//    if ((x / 10) > 0)
+//    {
+//        x2 = (x-x4*1000-x3*100) / 10;
+//        printf("x2:%d ", x2);
+//    }
+//    x1 = x % 10;
+//    printf("x1:%d ", x1);
+//    printf("\nx4:%d x3:%d x2:%d x1:%d\n", x4, x3, x2, x1);
+//
+//    if (x % (x4 + x3 + x2 + x1) == 0) answer = true;
+//    else answer = false;
+//    printf(answer? "true\n" : "false\n");
+//}
