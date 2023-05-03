6ad75a510bca16088f77202b9a95a08339e86f10
ChoMinkyung <58170545+ChoMinkyung@users.noreply.github.com>
2023-05-03 10:35:49 +0900

    20230503 Q2 문제 풀이

--- ConsoleApplication1/20230503_Q2.c
+++ ConsoleApplication1/20230503_Q2.c
@@ -6,10 +6,30 @@
 #include <stdbool.h>
 
 /*
+	다음 식을 만족하는 모든  A와 Z를 구하는 프로그램을 작성하라.
 
+	  A Z
+	+ Z A
+	------
+	  N N
+
+	N은 입력받아.
 */
 
 int main()
 {
-	
+	int n;
+	printf("자연수 n을 입력하세요 : ");
+	scanf_s("%d", &n);
+	printf("n=%d일때\n", n);
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (i+j==n)//((i * 10 + j) + (j * 10 + i) == n * 10 + n)
+			{
+				printf("A=%d, Z=%d\n", i, j);
+			}
+		}
+	}
 }
