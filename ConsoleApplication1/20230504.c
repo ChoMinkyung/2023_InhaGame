2b66f0663eb132813a7e6844bc2f5893dbe4ebbb
ChoMinkyung <58170545+ChoMinkyung@users.noreply.github.com>
2023-05-04 16:42:51 +0900

    20230504 Q5 문제 풀이

--- ConsoleApplication1/20230504.c
+++ ConsoleApplication1/20230504.c
@@ -1,143 +1,125 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <stdbool.h>
-//
-//int main()
-//{
-//	/*int ary[5];
-//
-//	ary[0] = 10;
-//	ary[1] = 20;
-//	ary[2] = ary[0] + ary[1];
-//
-//	scanf("%d", &ary[3]);
-//
-//	printf("%d\n", ary[2]);
-//	printf("%d\n", ary[3]);
-//	printf("%d\n", ary[4]);
-//
-//	int score[10];
-//	int i;
-//	int total = 0;
-//	double avg;
-//
-//	int size = sizeof(score) / sizeof(score[0]);
-//	for (i = 0; i < size; i++)
-//	{
-//		scanf("%d", &score[i]);
-//	}
-//
-//	for (i = 0; i < size; i++)
-//	{
-//		total += score[i];
-//	}
-//	avg = total / (double)size;
-//	for (i = 0; i < size; i++)
-//	{
-//		printf("%5d", score[i]);
-//	}
-//
-//	printf("\n");
-//	printf("평균:%.1f\n", avg);
-//
-//	char str[80] = "applejam";
-//	scanf("%s", str);
-//	printf("%s\n", str);
-//	for (int i = 0; i < 80; i++) printf("%c .",str[i]);
-//	printf("\n");
-//
-//	char str1[80] = "cat";
-//	char str2[80];
-//
-//	strcpy(str1, "tiger");
-//	strcpy(str2, str1);
-//	gets(str1);
-//	gets(str2);
-//	puts(str1);
-//	puts(str2);
-//	printf("%s, %s\n", str1, str2);
-//
-//	int a;
-//	double b;
-//	char c;
-//
-//	printf("int형 변수의 주소:%u\n", &a);
-//	printf("double형 변수의 주소:%u\n", &b);
-//	printf("char형 변수의 주소:%u\n", &c);
-//
-//	int a = 19;
-//	int* pA;
-//	pA = &a;
-//
-//	printf("%u\n", &a);
-//	printf("%x\n", &a);
-//	printf("%d\n", a);
-//	printf("%d\n", pA);
-//	printf("%d\n", *pA); // *pA==a
-//
-//	printf("*pA 다시 입력 :");
-//	scanf("%d", pA);
-//	printf("%d\n", a);
-//	printf("%d\n", *pA); // *pA==a
-//
-//	int a = 10, b = 15, total;
-//	double avg;
-//	int* pa, * pb;
-//	int* pt = &total;
-//	double* pg = &avg;
-//	pa = &a;
-//	pb = &b;
-//
-//	*pt = *pa + *pb;
-//	*pg = *pt / 2.0;
-//
-//	printf("두 정수의 값:%d, %d\n", *pa, *pb);
-//	printf("두 정수의 합:%d\n", *pt);
-//	printf("두 정수의 평균:%.1f\n", *pg);
-//
-//	
-//	
-//
-//	char ch;
-//	int in;
-//	double db;
-//
-//	char* pc = &ch;
-//	int* pi = &in;
-//	double* pd = &db;
-//
-//	printf("char형 변수의 주소 크기 : %d\n", sizeof(&ch));
-//	printf("int형 변수의 주소 크기 : %d\n", sizeof(&in));
-//	printf("double형 변수의 주소 크기 : %d\n", sizeof(&db));
-//	
-//	printf("char * 포인터의 크기 : %d\n", sizeof(pc));
-//	printf("int * 포인터의 크기 : %d\n", sizeof(pi));
-//	printf("duoble * 포인터의 크기 : %d\n", sizeof(pd));
-//
-//	printf("char * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pc));
-//	printf("int * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pi));
-//	printf("double * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pd));
-//	*/
-//
-//	char* s = "abcde";
-//	char* answer = (char*)malloc(strlen(s)*2);
-//	strcpy(answer, "");
-//	printf("%s", answer);
-//	int count = 0;
-//	for (int i = 0; i < strlen(s); i++)
-//	{
-//		if (i == strlen(s) / 2)
-//		{
-//			answer[count] = s[i];
-//			//printf("%c ", s[i]);
-//			//strncpy(, "", 1);
-//			count++;
-//		}
-//	}
-//	answer[count] = '\0';
-//	printf("%s", answer);
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+	/*int ary[5];
+
+	ary[0] = 10;
+	ary[1] = 20;
+	ary[2] = ary[0] + ary[1];
+
+	scanf("%d", &ary[3]);
+
+	printf("%d\n", ary[2]);
+	printf("%d\n", ary[3]);
+	printf("%d\n", ary[4]);
+
+	int score[10];
+	int i;
+	int total = 0;
+	double avg;
+
+	int size = sizeof(score) / sizeof(score[0]);
+	for (i = 0; i < size; i++)
+	{
+		scanf("%d", &score[i]);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		total += score[i];
+	}
+	avg = total / (double)size;
+	for (i = 0; i < size; i++)
+	{
+		printf("%5d", score[i]);
+	}
+
+	printf("\n");
+	printf("평균:%.1f\n", avg);
+
+	char str[80] = "applejam";
+	scanf("%s", str);
+	printf("%s\n", str);
+	for (int i = 0; i < 80; i++) printf("%c .",str[i]);
+	printf("\n");
+
+	char str1[80] = "cat";
+	char str2[80];
+
+	strcpy(str1, "tiger");
+	strcpy(str2, str1);
+	gets(str1);
+	gets(str2);
+	puts(str1);
+	puts(str2);
+	printf("%s, %s\n", str1, str2);
+
+	int a;
+	double b;
+	char c;
+
+	printf("int형 변수의 주소:%u\n", &a);
+	printf("double형 변수의 주소:%u\n", &b);
+	printf("char형 변수의 주소:%u\n", &c);
+
+	int a = 19;
+	int* pA;
+	pA = &a;
+
+	printf("%u\n", &a);
+	printf("%x\n", &a);
+	printf("%d\n", a);
+	printf("%d\n", pA);
+	printf("%d\n", *pA); // *pA==a
+
+	printf("*pA 다시 입력 :");
+	scanf("%d", pA);
+	printf("%d\n", a);
+	printf("%d\n", *pA); // *pA==a
+
+	int a = 10, b = 15, total;
+	double avg;
+	int* pa, * pb;
+	int* pt = &total;
+	double* pg = &avg;
+	pa = &a;
+	pb = &b;
+
+	*pt = *pa + *pb;
+	*pg = *pt / 2.0;
+
+	printf("두 정수의 값:%d, %d\n", *pa, *pb);
+	printf("두 정수의 합:%d\n", *pt);
+	printf("두 정수의 평균:%.1f\n", *pg);
+
+	
+	
+
+	char ch;
+	int in;
+	double db;
+
+	char* pc = &ch;
+	int* pi = &in;
+	double* pd = &db;
+
+	printf("char형 변수의 주소 크기 : %d\n", sizeof(&ch));
+	printf("int형 변수의 주소 크기 : %d\n", sizeof(&in));
+	printf("double형 변수의 주소 크기 : %d\n", sizeof(&db));
+	
+	printf("char * 포인터의 크기 : %d\n", sizeof(pc));
+	printf("int * 포인터의 크기 : %d\n", sizeof(pi));
+	printf("duoble * 포인터의 크기 : %d\n", sizeof(pd));
+
+	printf("char * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pc));
+	printf("int * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pi));
+	printf("double * 포인터가 가르키는 변수의 크기 : %d\n", sizeof(*pd));
+	*/
+
+	return 0;
+}
