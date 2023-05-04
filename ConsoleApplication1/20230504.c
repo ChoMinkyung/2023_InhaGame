0a812f6040cafd1fc5b3b4e5c22a2d3f66360693
ChoMinkyung <58170545+ChoMinkyung@users.noreply.github.com>
2023-05-04 11:06:32 +0900

    20230504 Q1 문제 풀이

---
+++ ConsoleApplication1/20230504.c
@@ -0,0 +1,46 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//
+//int main()
+//{
+//	/*int ary[5];
+//
+//	ary[0] = 10;
+//	ary[1] = 20;
+//	ary[2] = ary[0] + ary[1];
+//
+//	scanf("%d", &ary[3]);
+//
+//	printf("%d\n", ary[2]);
+//	printf("%d\n", ary[3]);
+//	printf("%d\n", ary[4]);*/
+//
+//	int score[10];
+//	int i;
+//	int total = 0;
+//	double avg;
+//
+//	int size = sizeof(score) / sizeof(score[0]);
+//	for (i = 0; i < size; i++)
+//	{
+//		scanf("%d", &score[i]);
+//	}
+//
+//	for (i = 0; i < size; i++)
+//	{
+//		total += score[i];
+//	}
+//	avg = total / (double)size;
+//	for (i = 0; i < size; i++)
+//	{
+//		printf("%5d", score[i]);
+//	}
+//
+//	printf("\n");
+//	printf("ЦђБе:%.1f\n", avg);
+//
+//	return 0;
+//}
